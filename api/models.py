e773a372080840b1485a002ef188989444beccf0
cygnu <crucia8@icloud.com>
2021-08-18 00:37:53 +0900

    feat: Create 'Tag' model in api/models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+import uuid
+
 # Create your models here.
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
