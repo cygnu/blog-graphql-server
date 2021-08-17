9ede040d67b779c2b74384b70ee9b82360486067
cygnu <crucia8@icloud.com>
2021-08-17 14:34:31 +0900

    feat: Reference from https://github.com/adamchainz/django-cors-headers. Configure 'django-cors-headers' in blog/settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'account.apps.AccoutnConfig',
     'api.apps.ApiConfig',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'localhost:3000/',
+    'localhost:3000',
+    '127.0.0.1:3000/',
+    '127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = 'blog.urls'
