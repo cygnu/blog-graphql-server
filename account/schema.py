1688fdc704c800e2c3e5cd97565e904b0381121b
cygnu <crucia8@icloud.com>
2021-08-18 16:19:19 +0900

    refactor: Implement 'UserProfile' in account/schema.py

--- account/schema.py
+++ account/schema.py
@@ -1,7 +1,13 @@
 from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
 
+from .models import Profile
+
 
 class UserType(DjangoObjectType):
     class Meta:
         model = get_user_model()
+
+class UserProfile(DjangoObjectType):
+    class Meta:
+        model = Profile
