6b058ee8749be06b5612fe65bb5d687b3ee65a4e
cygnu <crucia8@icloud.com>
2021-08-18 14:00:41 +0900

    feat: To make the model visible on the admin page, register the model with admin.site.register

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import (
+    User, LinkInBio, Profile
+)
+
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(LinkInBio)
+admin.site.register(Profile)
