cd9bf44cf9390e37fe65a6177c991153e4aa930a
cygnu <crucia8@icloud.com>
2021-08-19 12:03:23 +0900

    feat: Implement 'token_auth', 'verify_token' and 'refresh_token' mutations in accounnt/schema.py

--- account/schema.py
+++ account/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import graphql_jwt
 from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
 from blog.scalar import Uuid
@@ -69,3 +70,7 @@ class CreateUserMutation(graphene.Mutation):
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUserMutation.Field()
+
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
