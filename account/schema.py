25857a56150a6e02d33ff59d34b5a23fabcac842
cygnu <crucia8@icloud.com>
2021-08-19 11:53:34 +0900

    feat: Implement 'profile' and 'all_profiles' query in account/schema.py

--- account/schema.py
+++ account/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from django.contrib.auth import get_user_model
+from blog.scalar import Uuid
 from graphql_jwt.decorators import login_required
 
 from .models import Profile, LinkInBio
@@ -23,6 +24,13 @@ class Query(graphene.ObjectType):
     viewer = graphene.Field(UserProfile)
     all_users = graphene.List(UserType)
 
+    profile = graphene.Field(
+        UserProfile,
+        user_id=Uuid(),
+        username=graphene.String()
+    )
+    all_profiles = graphene.List(UserProfile)
+
     @login_required
     def resolve_viewer(self, info, **kwargs):
         return Profile.objects.get(user_prof=info.context.user.id)
@@ -30,3 +38,11 @@ class Query(graphene.ObjectType):
     @login_required
     def resolve_all_users(self, info, **kwargs):
         return get_user_model().objects.all()
+
+    @login_required
+    def resolve_profile(self, info, user_id, username):
+        return Profile.objects.filter(user_id=user_id, username=username).first()
+
+    @login_required
+    def resolve_all_profiles(self, info, **kwargs):
+        return Profile.objects.all()
